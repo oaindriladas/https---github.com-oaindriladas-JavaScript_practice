1da0a371b780b51e5e286a103e63152837b4ebeb
oaindrila.das <oaindrila.das@hotmail.com>
2023-01-14 10:26:02 +0530

    first commit

---
+++ factorial.js
@@ -0,0 +1,9 @@
+function factorial()
+{
+    let num=5, fact =1; // 5*4*3*2*1
+    for(let i=1; i<=num; i++)
+    {
+fact= fact*i;
+    }
+    console.log("Factorial is: "+fact)
+}
---
+++ merge.js
@@ -0,0 +1,26 @@
+var input= [12, 35, 72, 10, 55, 65, 101, 88];
+
+function merge(left, right) {
+  let sortedArr = [] 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift())
+    } else {
+      sortedArr.push(right.shift())
+    }
+  }
+  
+  return [...sortedArr, ...left, ...right];
+}
+
+function mergeSort(arr) {
+  // Base case
+  if (arr.length <= 1) return arr
+  let mid = Math.floor(arr.length / 2)
+  // Recursive calls
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+  return merge(left, right)
+}
+
+console.log(mergeSort(input));
